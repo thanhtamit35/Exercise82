de41bedb93ab30d18a749847a8b26ec4979298ab
thanhtamit35 <thanhtamdev35@gmail.com>
2022-09-06 14:50:43 +0700

    ListView in Android

---
+++ app/src/main/java/com/example/exercise82/DataModel.java
@@ -0,0 +1,47 @@
+package com.example.exercise82;
+
+public class DataModel {
+    private int info;
+    private String fullName;
+    private String gender;
+    private String address;
+
+    public DataModel(int info, String fullName, String gender, String address) {
+        this.info = info;
+        this.fullName = fullName;
+        this.gender = gender;
+        this.address = address;
+    }
+
+    public int getInfo() {
+        return info;
+    }
+
+    public void setInfo(int info) {
+        this.info = info;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
--- app/src/main/java/com/example/exercise82/MainActivity.java
+++ app/src/main/java/com/example/exercise82/MainActivity.java
@@ -1,14 +1,74 @@
 package com.example.exercise82;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imgAvatar;
+    TextView twFullName, twAddress;
+    RadioGroup radioGroup;
+    RadioButton rdoButton;
+    Button btnAddNew;
+    Button btnPrintList;
+    private final ArrayList<DataModel> employees = new ArrayList<>();
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
+
+        mapping();
+        addListenerOnButtonClick();
+    }
+
+    private void addListenerOnButtonClick() {
+        btnAddNew.setOnClickListener(view -> {
+            String fullName = twFullName.getText().toString();
+            String address = twAddress.getText().toString();
+
+            if (fullName.isEmpty() || address.isEmpty()) {
+                Toast.makeText(this, "Tên hoặc địa chỉ không được để trống!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            int selectedId = radioGroup.getCheckedRadioButtonId();
+            rdoButton = findViewById(selectedId);
+
+            String gender = rdoButton.getText().toString();
+            DataModel nv = new DataModel(R.mipmap.ic_launcher,fullName, gender, address);
+            employees.add(nv);
+
+            Toast.makeText(this, "Thêm mới thành công: " + nv.getFullName(),
+                    Toast.LENGTH_SHORT).show();
+        });
+
+        btnPrintList.setOnClickListener(view -> {
+
+            Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+//            intent.putStringArrayListExtra("data", data);
+            intent.putExtra("data", employees);
+            startActivity(intent);
+        });
+    }
+
+    private void mapping() {
+        imgAvatar = findViewById(R.id.img_avatar);
+        btnAddNew = findViewById(R.id.btn_add_new);
+        btnPrintList = findViewById(R.id.btn_print_list);
+        twFullName = findViewById(R.id.name);
+        twAddress = findViewById(R.id.address);
+        radioGroup = findViewById(R.id.radioGroup);
     }
 }
